17f2ecf7bfd67023a5f45cb32bf249ce4165a633
BITEYING <96273196+Biteliuying@users.noreply.github.com>
2022-02-12 14:32:22 +0800

    Add files via upload

---
+++ break.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int abc = 2, a = 1, b = 3, c = 5;
+	switch (a)
+	{
+	case 0:
+		printf("a=1 true\n");
+		break;
+	case 2:
+		printf("a=1 true\n");
+		break;
+	default:
+		printf("a=1 true\n");
+		break;
+	}
+
+}
